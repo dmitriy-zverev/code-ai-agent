610f19c38363fdfecb0d96d284d4ceb2fc86e5c4
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-06 13:19:22 +0300

    update code and add basic message history

--- main.py
+++ main.py
@@ -2,28 +2,39 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
-    user_prompt = sys.argv[1] if len(sys.argv) > 1 else None
+    load_dotenv()
 
-    if not user_prompt:
+    args = sys.argv[1:]
+
+    if not args:
+        print("Code AI Agent")
         print("Usage: uv run main.py <your_prompt>")
+        print("Example: uv run main.py \"How to build a calculator app?\"")
         sys.exit(1)
+    
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt
+        contents=messages
     )
-
+    print("Response:")
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
 
 if __name__ == "__main__":
     main()
